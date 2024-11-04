2216e7fa097fdb0e002b96d7479db7c60a645509
Ivan Aleksandrovskii <ivan.vered@gmail.com>
2024-11-05 05:30:28 +0700

    dice handler added

---
+++ handlers/__init__.py
@@ -0,0 +1,20 @@
+from aiogram import Router
+
+
+router = Router()
+
+from .on_start import router as on_start_router
+from .admin import router as admin_router
+
+from .promocode import router as promocode_router
+
+from .universal_page import router as universal_page_router
+
+router.include_router(on_start_router)
+router.include_router(admin_router)
+
+router.include_router(promocode_router)
+router.include_router(universal_page_router)
+
+from .dice import router as test_router
+router.include_router(test_router)
---
+++ handlers/dice.py
@@ -0,0 +1,41 @@
+# testing scripts, here got a working dice
+
+from typing import Any
+from icecream import ic
+import asyncio
+
+from aiogram import Router, types
+from aiogram import Bot
+from aiogram.fsm.context import FSMContext
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from aiogram.enums.dice_emoji import DiceEmoji
+
+from core.models import db_helper
+from .utils import send_or_edit_message
+
+
+router = Router()
+
+@router.callback_query(lambda c: c.data == "test")
+async def back_to_start(callback_query: types.CallbackQuery, state: FSMContext, bot: Bot, **kwargs: Any):
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="restart", url=None, callback_data="back_to_start")]])
+    
+    text = "Test"
+    ic(kwargs)
+    
+    from services.text_service import TextService
+    
+    async with db_helper.db_session() as session:
+        media = await TextService.get_default_media(session)
+    
+    await send_or_edit_message(callback_query.message, text, keyboard, media)
+    
+    # await asyncio.sleep(5)
+    
+    result = await bot.send_dice(callback_query.from_user.id, emoji=DiceEmoji.SLOT_MACHINE)
+    
+    await asyncio.sleep(4)
+    
+    await bot.send_message(callback_query.from_user.id, f"Result: {result.dice.value}")
