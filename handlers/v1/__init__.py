92a27531ba0913001d4f03ac0207f4fac426a048
Ivan Aleksandrovskii <ivan.vered@gmail.com>
2025-02-17 01:21:32 +0700

    v1 send test router disconnected for the optimisation (added file)

--- handlers/v1/__init__.py
+++ handlers/v1/__init__.py
@@ -16,8 +16,8 @@ router = Router()
 # router.include_router(promocode_router)
 # router.include_router(universal_page_router)
 
-from .dice import router as test_router
-router.include_router(test_router)
+# from .dice import router as test_router
+# router.include_router(test_router)
 
 # from .ai_chat import router as ai_chat_router
 # router.include_router(ai_chat_router)
@@ -31,15 +31,15 @@ router.include_router(ai_chat_with_memory_router)
 # from ..quiz import router as quiz_router
 # router.include_router(quiz_router)
 
-from .received_tests import router as recieved_tests_router
-router.include_router(recieved_tests_router)
+# from .received_tests import router as recieved_tests_router
+# router.include_router(recieved_tests_router)
 
 # from .broadcast_direct import router as direct_broadcast_router
 # router.include_router(direct_broadcast_router)
 
-from .send_test import router as send_test_router
-router.include_router(send_test_router)
+# from .send_test import router as send_test_router
+# router.include_router(send_test_router)
 
 
-from .ai_test_result_transcription import router as ai_test_result_transcription_router
-router.include_router(ai_test_result_transcription_router)
+# from .ai_test_result_transcription import router as ai_test_result_transcription_router
+# router.include_router(ai_test_result_transcription_router)
